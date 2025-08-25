235a103f2a35c30a06d862d6e4960c3220ccb370
LalaineHines <lalainehines@gmail.com>
2025-08-25 09:12:41 -0700

    added imports to eslint config

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+import globals from 'globals';
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
